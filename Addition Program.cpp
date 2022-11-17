2b29c8930b5a48f965844c98b9dc0dea175bd4eb
Mr-Code-Secure <118372301+Mr-Secure-Code@users.noreply.github.com>
2022-11-17 14:15:56 +0530

    Fourth Program

---
+++ Addition Program.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,add;
+    cout<<"Enter Two Value "<<endl;
+    cin>>a>>b;
+    add = a+b;
+    cout<<"Add is "<<add;
+    return 0;
+}
