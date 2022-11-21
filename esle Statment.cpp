ff048845d4e90755fe0686b39ef58ba44ef6ce3c
Mr-Code-Secure <118372301+Mr-Secure-Code@users.noreply.github.com>
2022-11-21 10:37:41 +0530

    Eighth Program

---
+++ esle Statment.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter First Value"<<endl;
+    cin>>a;
+    cout<<"Enter Second Value"<<endl;
+    cin>>b;
+    if (a>b)
+    {
+        cout<<"A is Greater";
+    }
+    else
+    {
+        cout<<"B is Greater";
+    }
+}
