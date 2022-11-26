d5b2dd346615ebdb1a4514430df6eb4b167f0912
Ritesh Sahu <118372301+Mr-Secure-Code@users.noreply.github.com>
2022-11-26 14:03:26 +0530

    do while 5

---
+++ Loops/do while loops/Revers Number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,rev=0,r;
+    cout<<"Enter A Value"<<endl;
+    cin>>n; 
+    while (n>0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+        
+    }
+    cout<<"Rev Number is = " <<rev<<endl;
+    
+}
