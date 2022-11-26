cfdb6529f65d5b274abf65cf3fd404c983c34fd3
Ritesh Sahu <118372301+Mr-Secure-Code@users.noreply.github.com>
2022-11-26 21:23:24 +0530

    Table Program

---
+++ Table .cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(void)
+{
+    int t,i=1,sum=0;
+    cout<<"Enter Table Number"<<endl;
+    cin>>t;
+    while(i<=10)
+    {
+        sum = t*i;
+        cout<<sum<<endl;
+        i++;
+    }
+    
+}
