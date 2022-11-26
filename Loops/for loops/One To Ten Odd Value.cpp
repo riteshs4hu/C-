1dafdb8ac562926274a937628a552116f2d38794
Ritesh Sahu <118372301+Mr-Secure-Code@users.noreply.github.com>
2022-11-26 13:18:46 +0530

    for 4

---
+++ Loops/for loops/One To Ten Odd Value.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int i;
+    for (i=1; i<=10; i++)
+    {
+        if (i%2!=0)
+        {
+            cout<<i<<endl;
+        }
+        i++;
+    }
+    return 0;
