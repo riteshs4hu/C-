2d5e8d760d55fc9692d063ac6cc3cffb58f7362c
Ritesh Sahu <118372301+Mr-Secure-Code@users.noreply.github.com>
2022-11-26 14:22:43 +0530

    while 6

---
+++ Loops/while loops/Palindrom Number.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,rev=0,r,rev2;
+    cout<<"Enter A Value"<<endl;
+    cin>>n;
+    rev2=n;
+    while (n>0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    if (rev==rev2)
+    {
+        cout<<"This Number is Palindrom"<<endl;
+    }
+    else
+    {
+        cout<<"This Number is Not Palindrom"<<endl;
+    }
+    return 0;
+}
